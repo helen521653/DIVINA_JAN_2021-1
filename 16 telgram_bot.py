c226bcb2fde33a42b561e5d1df651ec2f2fab37b
helen5216 <helenwaker666@gmail.com>
2021-01-04 19:06:28 +0300

    Update 16 telgram_bot.py

--- 16 telgram_bot.py
+++ 16 telgram_bot.py
@@ -1,8 +1,18 @@
 import telebot
 from myToken1 import TOKEN
-
+from random import randint
 
 bot = telebot.TeleBot(TOKEN)  #Создаём подключение к боту
+
+@bot.message_handler(commands=['start'])
+def komands_start(message):
+    bot.send_message(message.chat.id,'я умею много разного')
+@bot.message_handler(commands=['pass'])
+def komands_pass(message):
+    bot.send_message(message.chat.id,'сгенерирую пароль')
+    parol=randint(1000,9999)
+   bot.send_message(message.chat.id,str(parol))
+
 
 @bot.message_handler(content_types=['text'])
 def otvet_na_text(message):
