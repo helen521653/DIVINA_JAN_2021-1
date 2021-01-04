87bb086a6981bdf72b39fdff63d9324e02f7b0bc
Ekaterina Divina <ekaterinadivina@gmail.com>
2021-01-04 16:30:55 +0300

    Update 16 telgram_bot.py

--- 16 telgram_bot.py
+++ 16 telgram_bot.py
@@ -1,14 +1,13 @@
 import telebot
 
 
-bot = telebot.TeleBot('your token')  #Создаём подключение к боту
+bot = telebot.TeleBot('1470110849:AAEK5skHRPMBB-X1pSUDiNFq1K3gusdR8hY')  #Создаём подключение к боту
 
 
 @bot.message_handler(content_types=['text'])
 def otvet_na_text(message):
     print(message)
     bot.send_message(message.chat.id, message.text[::-1])
-
 
 
 
